19de8b760972f4468b54bc7b63e9c27e4fcb622b
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-06-04 03:30:25 -0300

    Add login system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  before_action :require_user, excpet: [:index ]
   def index
+  end
+
+  def dashboard
   end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: session_params[:email])
+
+    if !@user.nil? && @user.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      return redirect_to root_path
+    else
+      @user = User.new(session_params)
+      flash.now[:alert] = '&nbsp; Oops... email ou senha inválida'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:email, :password)
+  end
+  
+end
