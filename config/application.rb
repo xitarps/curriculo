07a6f834f622fe8309e99f7a62b94439f8bc8c39
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-08-17 10:16:30 -0300

    Time/Date fix

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Cvsys
   class Application < Rails::Application
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.time_zone = "America/Sao_Paulo"
+    ENV["TZ"] = "America/Sao_Paulo"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
---
+++ config/initializers/time_zone.rb
@@ -0,0 +1,2 @@
+# config/initializers/time_zone.rb
+Time.zone = "America/Sao_Paulo"
