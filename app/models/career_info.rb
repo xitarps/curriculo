38f678be8b305051fa7fcfd73debbaa8e163deb4
Xita Rps <xitarps@gmail.com>
2021-08-17 08:33:21 -0300

    Fix end date when still in progress

--- app/models/career_info.rb
+++ app/models/career_info.rb
@@ -2,6 +2,6 @@ class CareerInfo < ApplicationRecord
   validates :company, presence: true
   validates :work_function, presence: true
   validates :start_date, presence: true
-  validates :end_date, presence: true
-  # validates :description, presence: true
+  # validates :end_date, presence: true
+  validates :description, presence: true
 end
